4cb9d17da2964a3ab808414cede050e3cd1c5da5
sakshi87 <53863764+sakshi87@users.noreply.github.com>
2020-08-31 12:57:53 +0530

    Update tasks.py

--- tools/tasks.py
+++ tools/tasks.py
@@ -66,17 +66,17 @@ if 'CC' not in os.environ:
 if 'CXX' not in os.environ:
     os.environ['CXX'] = 'clang++'
 
-_framework_py = lambda xy: "/Library/Frameworks/Python.framework/Versions/{0}/bin/python{0}".format(xy)
-py_exes = {
-    '3.8' : _framework_py('3.8'),
-    '3.7' : _framework_py('3.7'),
-    '2.7' : _framework_py('2.7'),
-    '3.5' : _framework_py('3.5'),
-    '3.6' : _framework_py('3.6'),
-    'pypy': "/usr/local/bin/pypy",
-    'pypy3': "/usr/local/bin/pypy3",
-}
-egg_pys = {} # no more eggs!
+#_framework_py = lambda xy: "/Library/Frameworks/Python.framework/Versions/{0}/bin/python{0}".format(xy)
+#py_exes = {
+#    '3.8' : _framework_py('3.8'),
+#    '3.7' : _framework_py('3.7'),
+#    '2.7' : _framework_py('2.7'),
+#    '3.5' : _framework_py('3.5'),
+#    '3.6' : _framework_py('3.6'),
+#    'pypy': "/usr/local/bin/pypy",
+#    'pypy3': "/usr/local/bin/pypy3",
+#}
+#egg_pys = {} # no more eggs!
 
 default_py = '3.7'
 # all the Python versions to be built on linux
@@ -250,6 +250,7 @@ def manylinux(ctx, vs, upload=False, pythons=manylinux_pys):
 
     run("docker pull quay.io/pypa/manylinux1_x86_64")
     run("docker pull quay.io/pypa/manylinux1_i686")
+    run("docker pull quay.io/pypa/manylinux2014_aarch64")
     base_cmd = ' '.join([
         "docker",
         "run",
@@ -270,6 +271,7 @@ def manylinux(ctx, vs, upload=False, pythons=manylinux_pys):
     with cd(manylinux):
         run(base_cmd +  " quay.io/pypa/manylinux1_x86_64 /io/build_pyzmqs.sh")
         run(base_cmd +  " quay.io/pypa/manylinux1_i686 linux32 /io/build_pyzmqs.sh")
+        run(base_cmd +  " quay.io/pypa/manylinux2014_aarch64 linux /io/build_pyzmqs.sh")
     if upload:
         py = make_env(default_py, 'twine')
         run(['twine', 'upload', os.path.join(manylinux, 'wheelhouse', '*')])
