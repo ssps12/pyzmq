17fa941e1474b7fe7906e08edde8ff54524d05cf
ssps12 <38716735+ssps12@users.noreply.github.com>
2020-10-19 16:03:48 +0530

    Update tasks.py

--- tools/tasks.py
+++ tools/tasks.py
@@ -85,7 +85,6 @@ if platform.processor() != 'aarch64' and platform.processor() != 'x86_64':
 elif platform.processor() == 'aarch64':
     py_exes = {
         #'3.8' : "/home/travis/virtualenv/python3.8.0/bin/python"
-        echo $HOME
         '3.7' : "%r/virtualenv/python3.7.5/bin/python",
     }
 else:
