dfdfbdda7f1136b99544f37d1cceed4ed762346d
ssps12 <38716735+ssps12@users.noreply.github.com>
2020-10-19 13:12:55 +0530

    Update tasks.py

--- tools/tasks.py
+++ tools/tasks.py
@@ -85,12 +85,12 @@ if platform.processor() != 'aarch64' and platform.processor() != 'x86_64':
 elif platform.processor() == 'aarch64':
     py_exes = {
         #'3.8' : "/home/travis/virtualenv/python3.8.0/bin/python",
-        '3.7' : "/home/travis/virtualenv/python3.7.5/bin/python",
+        '3.7' : "$HOME/virtualenv/python3.7.5/bin/python",
     }
 else:
     py_exes = {
         #'3.8' : "/home/travis/virtualenv/python3.8.0/bin/python",
-        '3.7' : "/home/travis/virtualenv/python3.7.1/bin/python",
+        '3.7' : "$HOME/virtualenv/python3.7.1/bin/python",
     }
     
 egg_pys = {} # no more eggs!
